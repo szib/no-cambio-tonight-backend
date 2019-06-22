9194e05dbe3c5dfec196747661ac5cd86d4e3389
Ivan Szebenszki <szib77@gmail.com>
2019-06-22 16:16:06 +0100

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :show]
+      post '/login', to: 'users#login' # username, password => token
+      post '/validate', to: 'users#validate' # token => user || error
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create({
+  username: 'user1',
+  password: '11111',
+  password_confirmation: '11111'
+})
+u2 = User.create({
+  username: 'user2',
+  password: '22222',
+  password_confirmation: '22222'
+})
+
+u1.profile=Profile.new(full_name: 'User One', email: 'u1@boardgames.com')
+u2.profile=Profile.new(full_name: 'User Two', email: 'u2@boardgames.com')
