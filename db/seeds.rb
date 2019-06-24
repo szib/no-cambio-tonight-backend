ed14f8e065245503f500268729a517e336049418
Ivan Szebenszki <szib77@gmail.com>
2019-06-24 18:34:22 +0100

    Seed with some games from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-def create_users(number)
-  users = []
-  number.times do |idx|
-    user_idx = idx + 1
-    password = "#{user_idx}" * 6
-    user =   {
-      username: "user#{user_idx}",
-      password: password,
-      password_confirmation: password,
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      email: Faker::Internet.email,
-      member_since: Faker::Date.backward(90)
-    }
-    users << user
+
+unless Rails.env.production?
+  # Seed with games ["3xbCLNpbny", "fDn9rQjH9O", "GP7Y2xOUzj", "74f9mzbw9Y"]
+  connection = ActiveRecord::Base.connection
+  sql = File.read('db/games.sql')
+  statements = sql.split(/;$/)
+  statements.pop
+
+  ActiveRecord::Base.transaction do
+    statements.each do |statement|
+      connection.execute(statement)
+    end
   end
-  User.create(users)
+
+
+  def create_users(number)
+    users = []
+    number.times do |idx|
+      user_idx = idx + 1
+      password = "#{user_idx}" * 6
+      user =   {
+        username: "user#{user_idx}",
+        password: password,
+        password_confirmation: password,
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        member_since: Faker::Date.backward(90)
+      }
+      users << user
+    end
+    User.create(users)
+  end
+
+  create_users(10)
+
+  Event.create(organiser_id: 1, title: 'Event1')
+  Event.create(organiser_id: 2, title: 'Event2')
+
+  Attendance.create(attendee_id: 2, event_id: 1)
+  Attendance.create(attendee_id: 5, event_id: 1)
+  Attendance.create(attendee_id: 5, event_id: 2)
+  Attendance.create(attendee_id: 6, event_id: 2)
 end
-
-create_users(10)
-
-Event.create(organiser_id: 1, title: 'Event1')
-Event.create(organiser_id: 2, title: 'Event2')
-
-Attendance.create(attendee_id: 2, event_id: 1)
-Attendance.create(attendee_id: 5, event_id: 1)
-Attendance.create(attendee_id: 5, event_id: 2)
-Attendance.create(attendee_id: 6, event_id: 2)
