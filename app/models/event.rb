c1cf2f755bffa26d36a5ed7862bd8b05c35058e3
Ivan Szebenszki <szib77@gmail.com>
2019-06-25 12:33:28 +0100

    Add gamelist and number_of_attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
 
   has_many :attendances
   has_many :attendees, through: :attendances
+
+  def gamelist
+    attendances.map(&:gamepieces).flatten.map(&:game).uniq
+  end
+
+  def number_of_attendees
+    attendees.count
+  end
 end
