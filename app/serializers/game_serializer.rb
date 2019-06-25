af14a43a6576ae781aee13c8e20fab23e850d76c
Ivan Szebenszki <szib77@gmail.com>
2019-06-25 12:33:43 +0100

    Add number_of_owners

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -2,4 +2,6 @@ class GameSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :publisher, :year_published
   attributes :min_players, :max_players, :min_playtime, :max_playtime, :min_age, :average_user_rating
   attributes :image_thumb, :image_small, :image_medium, :image_large, :image_original, :rules_url
+
+  attribute :number_of_owners
 end
