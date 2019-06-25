94d848e58626da050e6d4e83c0fda1d5f4901fe1
Ivan Szebenszki <szib77@gmail.com>
2019-06-25 15:58:59 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :show]
-      resources :gamepieces, only: [:create, :destroy, :index], path: 'mygames'
-
       post '/login', to: 'users#login' # username, password => token
       post '/validate', to: 'users#validate' # token => user || error
 
+      resources :gamepieces, only: [:create, :destroy, :index], path: 'mygames'
       get '/games/search', to: 'games#search'
       post '/games/save', to: 'games#save' # probably not needed at all
+
+      resources :events, only: [:show, :index, :create] do
+        get '/cancel', to: 'events#cancel'
+      end
+
     end
   end
 end
