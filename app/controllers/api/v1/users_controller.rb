04ab8f5aa12016647e5dfc6061febb9e3a435ff4
Ivan Szebenszki <szib77@gmail.com>
2019-06-26 10:01:52 +0100

    Add profile, events, destroy to usercontroller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::UsersController < ApplicationController
   skip_before_action :require_login, only: %i[login create]
 
@@ -5,26 +7,33 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_registration_params)
     user.member_since = Date.today
     if user.save
-      render json: { token: issue_token({ id: user.id }) }
+      render json: { token: issue_token(id: user.id) }
     else
       render json: { error: 'Cannot create user.' }, status: 400
     end
   end
-  
 
   def show
     user = User.find_by(id: params[:id])
     if user
       render json: user
     else
-      render json: { error: "User not found."}, status: 404
+      render json: { error: 'User not found.' }, status: 404
+    end
+  end
+
+  def destroy
+    if current_user.destroy
+      render json: { status: 'DELETED' }, status: 200
+    else
+      render json: { error: 'User not found.' }, status: 404
     end
   end
 
   def login
     user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      render json: { token: issue_token({ id: user.id }) }
+    if user&.authenticate(params[:password])
+      render json: { token: issue_token(id: user.id) }
     else
       render json: { error: 'Invalid username/password combination.' }, status: 401
     end
@@ -33,16 +42,26 @@ class Api::V1::UsersController < ApplicationController
   def validate
     user = current_user
     if user
-      render json: { token: issue_token({ id: user.id }) }
+      render json: { token: issue_token(id: user.id) }
     else
       render json: { error: 'Invalid token.' }, status: 401
     end
   end
 
+  def profile
+    render json: current_user
+  end
+
+  # TODO
+  def patch_profile; end
+
+  def events
+    render json: current_user.organised_events
+  end
+
   private
 
-    def user_registration_params
-      params.permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
-    end
-    
+  def user_registration_params
+    params.permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
+  end
 end
