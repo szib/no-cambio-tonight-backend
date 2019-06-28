a5658fa7177dc0c238a3d1d8cb607638938b62bd
Ivan Szebenszki <szib77@gmail.com>
2019-06-28 09:18:33 +0100

    Add bga_id to game serialiser

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :publisher, :year_published
+  attributes :id, :bga_id, :name, :description, :publisher, :year_published
   attributes :min_players, :max_players, :min_playtime, :max_playtime, :min_age, :average_user_rating
   attributes :image_thumb, :image_small, :image_medium, :image_large, :image_original, :rules_url
 
