e62b2dc507dd2e27d15b2e89733eccd1478aa13e
Ivan Szebenszki <szib77@gmail.com>
2019-06-29 18:09:37 +0100

    Add number of attendees and capacity to serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :location, :date_time, :is_cancelled 
+  attributes :id, :title, :location, :date_time, :is_cancelled, :capacity, :number_of_attendees
   has_one :organiser, serializer: UserSerializer
   has_many :attendees, serializer: UserSerializer
   has_one :gamelist
