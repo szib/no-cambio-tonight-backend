e4e389b8060f344fe7196b8754688ac66a479f81
Ivan Szebenszki <szib77@gmail.com>
2019-07-01 13:22:36 +0100

    Add attendees and gamelist to event

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -5,6 +5,8 @@ class EventSerializer < ActiveModel::Serializer
   attributes :is_current_user_attending
   attributes :is_current_user_organising
   has_one :organiser, serializer: UserSerializer
+  has_many :attendees
+  has_one :gamelist
 
   def is_current_user_attending
     !object.attendees.find_by(id: current_user.id).nil?
